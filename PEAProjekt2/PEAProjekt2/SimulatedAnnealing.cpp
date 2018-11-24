ed50bcbfb20112bbed1096a7e59d2a86517145a0
Julia Zajusz <julia.zajusz@fingo.pl>
2018-11-24 18:57:22 +0100

    Comments deleted

--- PEAProjekt2/PEAProjekt2/SimulatedAnnealing.cpp
+++ PEAProjekt2/PEAProjekt2/SimulatedAnnealing.cpp
@@ -9,46 +9,18 @@ SimulatedAnnealing::SimulatedAnnealing()
 SimulatedAnnealing::SimulatedAnnealing(vector_matrix m)
 {
 	neighborhoodMatrix = m;
-	// path = new int[neighborhoodMatrix.nVertices + 1];
-	// visited = new bool[neighborhoodMatrix.nVertices];
 	find_path(m, 20, 0.9);
 }
-
-void SimulatedAnnealing::save_path(int* currentPath)
-{
-}
-
-// vector<int> SimulatedAnnealing::getRandomNeigboringSolution(vector<int> path)
-// {
-// 	int indexOfFirstSwappedVerticle = std::rand() % path.size();
-// 	int indexOfSecondSwappedVerticle = std::rand() % path.size();
-// 	while(indexOfFirstSwappedVerticle == indexOfSecondSwappedVerticle)
-// 	{
-// 		indexOfSecondSwappedVerticle = std::rand() % path.size();
-// 	}
-// 	int swapBuffor = path[indexOfFirstSwappedVerticle];
-// 	path[indexOfFirstSwappedVerticle] = path[indexOfSecondSwappedVerticle];
-// 	path[indexOfSecondSwappedVerticle] = swapBuffor;
-// 	return path;
-// }
 
 vector<int> SimulatedAnnealing::getRandomNeigboringSolution(vector<int> path)
 {
 	vector<int> neighboringPath;
-	// // int indexOfFirstSwappedVerticle = std::rand() % (path.size()-1);
-	// int indexOfFirstSwappedVerticle = std::rand() % path.size();
-	// // int indexOfSecondSwappedVerticle = (indexOfFirstSwappedVerticle + path.size() / 2) % (path.size()-1);
-	// int indexOfSecondSwappedVerticle = (indexOfFirstSwappedVerticle + path.size() / 2) % path.size();
-
 	int indexOfFirstSwappedVerticle = std::rand() % path.size();
 	int indexOfSecondSwappedVerticle = std::rand() % path.size();
 	while(indexOfFirstSwappedVerticle == indexOfSecondSwappedVerticle)
 	{
 		indexOfSecondSwappedVerticle = std::rand() % path.size();
 	}
-
-
-	// cout << "index pierwszy: " << indexOfFirstSwappedVerticle << ", index drugi: " << indexOfSecondSwappedVerticle << endl;
 
 	for (int i = 0; i < path.size(); i++)
 	{
@@ -72,21 +44,10 @@ vector<int> SimulatedAnnealing::getRandomNeigboringSolution(vector<int> path)
 			}
 			else
 			{
-				// neighboringPath.push_back(path[(indexOfSecondSwappedVerticle +indexOfFirstSwappedVerticle - i + 1) % path.size()]);
 				neighboringPath.push_back(path[(indexOfFirstSwappedVerticle + 1 - (i - indexOfSecondSwappedVerticle) + path.size()) % path.size()]);
 			}
 		}
 	}
-
-	// cout << "path:" << endl;
-	// for (int x : path)
-	// cout << x << " ";
-	// cout << endl;
-	// cout << "firstIndex: " << indexOfFirstSwappedVerticle << ", second index: " << indexOfSecondSwappedVerticle << endl;
-	// cout << "neighboringPath:" << endl;
-	// for (int x : neighboringPath)
-	// 	cout << x << " ";
-	// cout << endl;
 
 	return neighboringPath;
 }
@@ -101,20 +62,16 @@ int SimulatedAnnealing::calculatePathCost(vector<int> path)
 	for(int i = 0; i < path.size()-1; i++)
 	{
 		cost += neighborhoodMatrix.neighborhoodMatrix[path[i]][path[i + 1]];
-		// cout<< neighborhoodMatrix.neighborhoodMatrix[path[i]][path[i + 1]]<< "+";
 	}
 	cost += neighborhoodMatrix.neighborhoodMatrix[path[path.size()-1]][path[0]];
-	// cout<< neighborhoodMatrix.neighborhoodMatrix[path[path.size()-1]][path[0]]<<endl;
 	return cost;
 }
 
 
 
 
-
 void SimulatedAnnealing::find_path(vector_matrix m, int iterations, float tempCoefficient)
 {
-	int licznikZmianSciezki = 0;
 	//wyznaczenie losowego rozwiązania początkowego
 	vector<int> sPath;
 		for (int i = 0; i < neighborhoodMatrix.nVertices; i++)
@@ -128,41 +85,16 @@ void SimulatedAnnealing::find_path(vector_matrix m, int iterations, float tempCo
 
 	path = sPath;//sB
 	
-	// sPath.push_back(sPath[0]);
-	// for (int x : sPath)
-	// 	cout << x << " ";
-	// cout<<endl;
-	
-	// for (int x : sPath)  //wypisz nowa sciezke
-	// 	cout << x << " ";
-	// cout << endl;
-	// cout << "waga Sciezki: " << calculatePathCost(sPath);
-	// cout << endl;
-	
 	int warunek = 0;
 	while (warunek<100)
 	{
-		for (int i = 0; i < 1*iterations; i++)
+		for (int i = 0; i < iterations; i++)
 		{
 			vector<int> s1Path = getRandomNeigboringSolution(sPath);  //wygenerowanie sasiedniego rozwiazania
-	
 	
 			int sCost = calculatePathCost(sPath);
 			int s1Cost = calculatePathCost(s1Path);
 			int sBCost = calculatePathCost(path); //sB cost
-	
-	
-	
-			// for (int x : s1Path)  //wypisz nowa sciezke
-			// 	cout << x << " ";
-			// cout << endl;
-			// cout << "waga Sciezki: " << s1Cost;
-			// if (s1Cost <= 256) {
-			// 	cout << " !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!";
-			// }
-			// cout << endl<< endl;
-	
-	
 	
 			if(s1Cost<sBCost)  //sB
 			{
@@ -172,30 +104,21 @@ void SimulatedAnnealing::find_path(vector_matrix m, int iterations, float tempCo
 			int costDifference = s1Cost - sCost;
 			if (costDifference < 0)
 			{
-				// cout << "poprawa "<<i << endl;
 				sPath = s1Path;
 			}
 			else
 			{
-				double rand = (1.0 + std::rand() % (99)) / 100;
-
-				// cout << endl << "sCost: " << sCost << " s1Cost: " << s1Cost << " " << costDifference << " " << kb * temperature;
-				// cout<< " rand: " << rand << " eksponenta: " << exp(0.0 -costDifference / temperature) << endl;
-				
+				double rand = (1.0 + std::rand() % (99)) / 100;			
 
 				if (rand < exp((0.0-costDifference) / temperature))
 				{
-					// cout << endl << "zamiana sciezek<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<" << endl;
 					sPath = s1Path;
-					licznikZmianSciezki++;
 				}
 			}
 		}
 		temperature = tempCoefficient * temperature;
 		warunek++;
 	}
-
-	cout << "Zmian sciezki nastąpiło: " << licznikZmianSciezki<<endl;
 }
 
 void SimulatedAnnealing::print_result()
--- PEAProjekt2/PEAProjekt2/SimulatedAnnealing.h
+++ PEAProjekt2/PEAProjekt2/SimulatedAnnealing.h
@@ -3,8 +3,6 @@
 #include "matrix.h"
 #include "VectorMatrix.h"
 
-const double kb = 1.38064852 * 1 / 1000000000000000000;
-
 class SimulatedAnnealing {
 	vector_matrix neighborhoodMatrix;
 	vector<int> path;
@@ -14,7 +12,6 @@ class SimulatedAnnealing {
 public:
 	SimulatedAnnealing();
 	SimulatedAnnealing(vector_matrix m);
-	void save_path(int* currentPath);
 	vector<int> getRandomNeigboringSolution(vector<int> path);
 	int calculatePathCost(vector<int> path);
 	void find_path(vector_matrix m, int iterations, float tempCoefficient);
